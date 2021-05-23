bda49b228c2075c67013a5ce8e7f6f7bdcd1702a
al394495 <al394495@uji.es>
2021-05-23 23:46:48 +0200

    Cambios en mi fichero js de avoid

--- Antartic/js/avoid.js
+++ Antartic/js/avoid.js
@@ -5,13 +5,27 @@ let avoidState = {
 }
 
 function loadAvoidAssets() {
+    loadAvoidSprites();
+    loadAvoidImages();
+}
 
+function loadAvoidImages() {
+    game.load.image('backgAvoid','assets/imgs/AvoidBackground.png');
 }
 
 function createAvoidLevel() {
+    //Set World bounds (arreglar que no son los que tocan)
+    game.world.setBounds(0,0,GAME_WIDTH,GAME_HEIGHT);
 
+    //Background
+    let bgAvoid = game.add.tileSprite(0,0, game.world.width, game.world.height, 'backgAvoid');
+    bgAvoid.scrollFactorX = 0.7;
 }
 
 function updateAvoidLevel() {
     
+}
+
+function loadAvoidSprites() {
+
 }
