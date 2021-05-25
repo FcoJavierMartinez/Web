a1fc2515a81806ff7789a981446d619448700e05
al394495 <al394495@uji.es>
2021-05-26 01:07:06 +0200

    Actualizaciones

--- Antartic/js/avoid.js
+++ Antartic/js/avoid.js
@@ -11,6 +11,7 @@ function loadAvoidAssets() {
 
 function loadAvoidImages() {
     game.load.image('backgAvoid','assets/imgs/AvoidBackground.png');
+    game.load.image('prota', 'assets/imgs/falsoprota.png');
 }
 
 function createAvoidLevel() {
@@ -20,6 +21,18 @@ function createAvoidLevel() {
     //Background
     let bgAvoid = game.add.tileSprite(0,0, game.world.width, game.world.height, 'backgAvoid');
     bgAvoid.scrollFactorX = 0.7;
+
+    //Create player
+    player = game.add.image(35,410,'prota');
+    player.anchor.setTo(0.5,0.5);
+    game.physics.arcade.enable(player);
+
+    //Physic properties
+    player.body.gravity.y = PLAYER_BODY_GRAVITY;
+    player.body.collideWorldBounds = true;
+
+    //Camera follows the player
+    game.camera.follow(player);
 }
 
 function updateAvoidLevel() {
--- Antartic/js/main.js
+++ Antartic/js/main.js
@@ -1,6 +1,8 @@
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
+const PLAYER_BODY_GRAVITY = 250;
 let global_score = 0;
+let player;
 let game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, 'game');
 
 window.onload = StartGame();
