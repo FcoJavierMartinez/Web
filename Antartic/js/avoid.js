6560f8f6a2a9dd49b4d24ce524410b3ce2eb5214
al394495 <al394495@uji.es>
2021-05-27 15:21:10 +0200

    Actualizaciones

--- Antartic/js/avoid.js
+++ Antartic/js/avoid.js
@@ -47,11 +47,11 @@ function createAvoidLevel() {
 
 function updateAvoidLevel() {
     //Movimiento del pingüino
-    player.body.velocity.x = 0;
+    /*player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
         player.body.velocity.x = -PLAYER_VELOCITY;
     } else if (cursors.right.isDown) {
         player.body.velocity.x = PLAYER_VELOCITY;
-    }
+    }*/
 }
--- Antartic/js/main.js
+++ Antartic/js/main.js
@@ -4,16 +4,18 @@ const PLAYER_BODY_GRAVITY = 250;
 const PLAYER_VELOCITY = 150;
 let global_score = 0;
 let player, cursors;
-let game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, 'game');
+let game;
 
 window.onload = StartGame();
 
 function StartGame() {
+    game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, 'game');
+
     game.state.add("init",initState);
     game.state.add("instructions", instructionState);
     game.state.add("avoid",avoidState);
     game.state.add("timed", timedState);
     game.state.add("platform",platformState);
 
-    game.state.start('timed');
+    game.state.start('init');
 }
