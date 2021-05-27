755311217cc413ecb63209c5066d8f895ef9e6c8
al394495 <al394495@uji.es>
2021-05-27 16:45:46 +0200

    Update avoid.js

--- Antartic/js/avoid.js
+++ Antartic/js/avoid.js
@@ -30,7 +30,7 @@ function createAvoidLevel() {
     bgAvoid.scrollFactorX = 0.7;
 
     //Create player
-    player = game.add.image(35,410,'prota');
+    player = game.add.sprite(35,410,'prota');
     player.anchor.setTo(0.5,0.5);
     game.physics.arcade.enable(player);
 
@@ -47,11 +47,11 @@ function createAvoidLevel() {
 
 function updateAvoidLevel() {
     //Movimiento del pingüino
-    /*player.body.velocity.x = 0;
+    player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
         player.body.velocity.x = -PLAYER_VELOCITY;
     } else if (cursors.right.isDown) {
         player.body.velocity.x = PLAYER_VELOCITY;
-    }*/
+    }
 }
