0b7f012bbe45eddf4466c4e15ee65cf943aab160
al394495 <al394495@uji.es>
2021-05-28 19:51:04 +0200

    Update avoid.js

--- Antartic/js/avoid.js
+++ Antartic/js/avoid.js
@@ -1,5 +1,10 @@
 const AVOID_WIDTH = 2400;
 const AVOID_HEIGHT = 600;
+const CHUZOS_GROUP_SIZE = 200;
+const TIMER_RHYTHM = 0.1 * Phaser.Timer.SECOND;
+let chuzos;
+let chuzosProbability = 0.4;
+let chuzosVelocity = 200;
 
 let avoidState = {
     preload: loadAvoidAssets,
@@ -16,6 +21,7 @@ function loadAvoidImages() {
     game.load.image('backgBackAvoid','assets/imgs/AvoidBackground_back.png');
     game.load.image('backgMediumAvoid', 'assets/imgs/AvoidBackground_medium.png');
     game.load.image('backgFrontAvoid', 'assets/imgs/AvoidBackground_front.png');
+    game.load.image('chuzo', 'assets/imgs/Chuzo.png');
 }
 
 function loadAvoidSprites() {
@@ -51,6 +57,37 @@ function createAvoidLevel() {
     player.animations.add('right',[19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37],true);
     //Controls
     cursors = game.input.keyboard.createCursorKeys();
+
+    createChuzos(CHUZOS_GROUP_SIZE);
+}
+
+function createChuzos(size) {
+    chuzos = game.add.group();
+    chuzos.enableBody = true;
+    chuzos.createMultiple(size, 'chuzo');
+    chuzos.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetMember);
+    chuzos.callAll('anchor.setTo', 'anchor',0.5,1.0);
+    game.time.events.loop(TIMER_RHYTHM, activateChuzo, this);
+}
+
+function activateChuzo() {
+    if(Math.random() < chuzosProbability) {
+        let chuzo = chuzos.getFirstExists(false);
+        if (chuzo) {
+            let gw = game.world.width;
+            let cw = chuzo.body.width;
+            let w = gw - cw;
+            let x = Math.floor(Math.random() * w);
+            let z = cw / 2 + x;
+            chuzo.reset(z, 0);
+            chuzo.body.velocity.x=0;
+            chuzo.body.velocity.y = chuzosVelocity;
+        }
+    }
+}
+
+function resetMember(item) {
+    item.kill();
 }
 
 function updateAvoidLevel() {
